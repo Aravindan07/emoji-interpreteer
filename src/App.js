e869607639817a847aff0ae595ea5998fa25a58e
Aravindan07 <50622300+Aravindan07@users.noreply.github.com>
2020-12-07 16:00:01 +0530

    add: Added emoji details feature

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
 import React, { useState } from "react";
 import "./App.css";
+import { flagEmojis } from "./constants";
 
-const emojiDictionary = {
-  "😂": "Face with tears of joy",
-  "😎": "Smiling face with sunglasses",
-  "😍": "Smiling face with heart eyes",
-  "🎁": "Wrapped gift",
-  "😅": "Grinning face with sweat"
-};
-
-let emojiList = Object.keys(emojiDictionary);
+let emojiList = Object.keys(flagEmojis);
 
 export default function App() {
   // const [value, setValue] = useState("");
   const [emojiValue, setEmojiValue] = useState("");
   const emojiSearchHandler = (e) => {
     let userInput = e.target.value;
-    let meaning = emojiDictionary[userInput];
-    if (meaning === undefined) {
-      meaning = "Sorry, we don't have this in our dictionary";
-    }
+    let meaning = flagEmojis[userInput];
     setEmojiValue(meaning);
   };
   const emojiClickHandler = (item) => {
-    let meaning = emojiDictionary[item];
+    let meaning = flagEmojis[item];
     setEmojiValue(meaning);
   };
   return (
-    <div className="App">
-      <h1>Inside out</h1>
-      <input type="text" onChange={emojiSearchHandler} />
-      <h2>{emojiValue}</h2>
-      <h3>Emojis we know</h3>
-      {emojiList.map((item, index) => {
-        return (
-          <span
-            style={{ cursor: "pointer", fontSize: "2rem", padding: "0.5rem" }}
-            key={index}
-            onClick={() => emojiClickHandler(item)}
-          >
-            {item}
-          </span>
-        );
-      })}
+    <div className="wrapper">
+      <h1>Transportation & Places Interpreter</h1>
+      <div className="input-wrap">
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={emojiSearchHandler}
+          placeholder="Enter an emoji"
+        />
+        <div className="input-wrap output">
+          {emojiValue === "" || emojiValue === undefined
+            ? "Your output will be showed here"
+            : emojiValue}
+        </div>
+        OR
+        <p> Click on any emoji from below </p>
+        <div className="show-emojis">
+          {emojiList.map((item, index) => {
+            return (
+              <span
+                style={{
+                  cursor: "pointer",
+                  fontSize: "2rem",
+                  padding: "0.5rem",
+                }}
+                key={index}
+                onClick={() => emojiClickHandler(item)}
+              >
+                {item}
+              </span>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
